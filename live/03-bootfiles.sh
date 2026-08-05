fd540183fe4dbc4fa4046882f06966dabc0f484b
romeohorvath <271563335+romeohorvath@users.noreply.github.com>
2026-08-05 22:17:00 +0200

    fix: generate GRUB BIOS/UEFI boot images (eltorito.img, efiboot.img) for xorriso -as mkisofs

--- live/03-bootfiles.sh
+++ live/03-bootfiles.sh
@@ -28,4 +28,10 @@ menuentry "Xuzuntu Omni $DISTRO_VERSION - Live (basic)" {
     initrd /live/initrd.img
 }
 GRUB
+# Generate GRUB BIOS boot image (eltorito.img)
+grub-mkimage     -O i386-pc     -o "$STAGE/boot/grub/eltorito.img"     -p "/boot/grub"     biosdisk part_gpt part_msdos fat iso9660 linux normal configfile loopback search
+
+# Generate GRUB UEFI boot image (efiboot.img)
+grub-mkimage     -O x86_64-efi     -o "$STAGE/boot/grub/efiboot.img"     -p "/boot/grub"     fat iso9660 part_gpt part_msdos linux normal configfile loopback search efi_gop efi_uga
+
 log "Live: boot configuration ready"
